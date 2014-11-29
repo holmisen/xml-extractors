f34bcb4676e739268671650f81fa49be4ec9b73f
Johan Holmquist <holmisen@gmail.com>
2014-11-29 19:13:30 +0100

    [Extra] added

---
+++ src/Text/XML/Light/Extractors/Extra.hs
@@ -0,0 +1,15 @@
+module Text.XML.Light.Extractors.Extra where
+
+
+import Safe (readMay)
+import Text.XML.Light.Extractors
+
+
+float :: (Floating a, Read a) => String -> Either Err a
+float s = 
+  maybe (Left $ ErrMsg ("Expected float, found: " ++ show s)) return (readMay s)
+
+
+integer :: (Integral a, Read a) => String -> Either Err a
+integer s = 
+  maybe (Left $ ErrMsg ("Expected integer, found: " ++ show s)) return (readMay s)
