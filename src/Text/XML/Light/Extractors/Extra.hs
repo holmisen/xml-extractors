5378cbea74a1c1d8bc0b8e39f511468f4c70184a
Johan Holmquist <holmisen@gmail.com>
2016-07-12 12:01:14 +0200

    Replaced Safe.readMay with Text.Read.readMaybe

--- src/Text/XML/Light/Extractors/Extra.hs
+++ src/Text/XML/Light/Extractors/Extra.hs
@@ -1,15 +1,15 @@
 module Text.XML.Light.Extractors.Extra where
 
-import Safe (readMay)
+import Text.Read (readMaybe)
 
 
 -- | Reads a floating point value or return @'Left' "float"@ if
 -- the read fails.
 float :: (Floating a, Read a) => String -> Either String a
-float = maybe (Left "float") return . readMay
+float = maybe (Left "float") return . readMaybe
 
 
 -- | Reads an integer value or return @'Left' "integer"@ if the read
 -- fails.
 integer :: (Integral a, Read a) => String -> Either String a
-integer = maybe (Left "integer") return . readMay
+integer = maybe (Left "integer") return . readMaybe
